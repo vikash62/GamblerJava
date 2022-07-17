4c1c963655c955ea0583f71e9635c8108cb47566
Vikash <vikashpathak041@gmail.com>
2022-07-17 09:41:16 +0530

    starting gambler programe java

---
+++ src/com/blz/Gambler.java
@@ -0,0 +1,11 @@
+package com.blz;
+
+public class Gambler {
+    public static final int BET = 1;
+    public static final int AMOUNT = 100;
+    public static void main(String[] args) {
+
+        int won = 0;
+        System.out.println("Initial Stake is " +AMOUNT+ "$ and bet is " +BET+"$");
+    }
+}
