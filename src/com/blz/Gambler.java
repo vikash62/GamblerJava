ea8ee0eb0c9a45b5e05b8494136f4fa1f8fa5353
Vikash <vikashpathak041@gmail.com>
2022-07-17 09:46:56 +0530

    starting gambler programe java uc 3

--- src/com/blz/Gambler.java
+++ src/com/blz/Gambler.java
@@ -3,16 +3,22 @@ package com.blz;
 public class Gambler {
     public static final int BET = 1;
     public static final int AMOUNT = 100;
+
     public static void main(String[] args) {
 
-        int won = 0;
-        int bet = (int) (Math.floor(Math.random()*10)%2);
-        if (bet == 1){
-            System.out.println("Won $1");
-            System.out.println("Initial Stake is $" +AMOUNT+ ", won $1 "+"and total is $100");
-        }else {
-            System.out.println("Loss $1");
-            System.out.println("Initial Stake is $" + AMOUNT + ", lost $1 "+"and total is $99");
+        int stake = 100;
+
+        while (stake > (0.5 * AMOUNT) && stake < (1.5 * AMOUNT)) {
+            int bet = (int) (Math.floor(Math.random() * 10) % 2);
+            if (bet == 1) {
+                System.out.println("Won $1");
+                System.out.println("Initial Stake is $" + stake + ", won $1 " + "and total is " + "$"+(stake + BET));
+                stake++;
+            } else if (bet == 0) {
+                System.out.println("Loss $1");
+                System.out.println("Initial Stake is $" + stake + ", lost $1 " + "and total is " + "$"+(stake - BET));
+                stake--;
+            }
         }
     }
 }
